dee74920d4d97068f0e7f131b05a936fa07ab7d5
Rayhan Egar <rresnn@gmail.com>
2024-06-16 19:12:39 +0700

    vite.config update for mixed content render

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -11,10 +12,23 @@ export default defineConfig({
         vue({
             template: {
                 transformAssetUrls: {
-                    base: null,
+                    base:null,
                     includeAbsolute: false,
                 },
             },
         }),
     ],
+    server:{
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    build: {
+        outDir: path.resolve(__dirname, 'public/build'), // Update your build path
+        emptyOutDir: true,
+        manifest: true, // Add a manifest to map the assets
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        }
+    }
 });
